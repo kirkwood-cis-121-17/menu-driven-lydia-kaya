2584f1bac8957228b4d5263beea3632fab4f03fe
Lydia Hecker <lydia-hecker@student.kirkwood.edu>
2017-11-14 14:46:25 +0000

    Added a while loop

--- languages.py
+++ languages.py
@@ -1,20 +1,5 @@
 import time
 
-def main():
-    print("1) Japanese")
-    print("2) French")
-    print("3) German")
-    choice = input("What language?: ")
-    if choice == "1":
-        sentence_japanese()
-    elif choice == "2":
-        sentence_french()
-    elif choice == "3":
-        sentence_german()
-    else:
-        print("Please enter a number from 1-3 and try again.")
-        main()
-    
 def sentence_japanese():
     print("1) I want a cat.")
     print("2) I'm hungry.")
@@ -24,17 +9,14 @@ def sentence_japanese():
         time.sleep(1)
         print("Watashi neko ga hoshii")
         print("私猫が欲しい")
-        again()
     elif choice == "2":
         time.sleep(1)
         print("Onaka ga suita")
         print("お腹がすいた")
-        again()
     elif choice == "3":
         time.sleep(1)
         print("Ima bennkyo o shimasu")
         print("今勉強します")
-        again()
     else:
         print("Try again. Enter a valid number.")
         sentence_japanese()
@@ -47,19 +29,16 @@ def sentence_french():
     if choice == "1":
         time.sleep(1)
         print("Je veux un chat.")
-        again()
     elif choice == "2":
         time.sleep(1)
         print("J'ai faim.")
-        again()
     elif choice == "3":
         time.sleep(1)
         print("J'étudie")
-        again()
     else:
         print("Try again. Enter a valid number.")
         sentence_french()
-
+            
 def sentence_german():
     print("1) I want a cat.")
     print("2) I'm hungry.")
@@ -68,26 +47,37 @@ def sentence_german():
     if choice == "1":
         time.sleep(1)
         print("Ich möchte eine Katze")
-        again()
     elif choice == "2":
         time.sleep(1)
         print("Ich habe Hunger")
-        again()
     elif choice == "3":
         time.sleep(1)
         print("Ich studiere")
-        again()
     else:
         print("Try again. Enter a valid number.")
         sentence_german()
 
-def again():
-    print("")
-    choice = input("Would you like to translate another sentence? yes or no ")
-    if choice.lower() == "yes".lower():
-        main()
+choice = "continue"
+while choice == "continue":
+    print("1) Japanese")
+    print("2) French")
+    print("3) German")
+    choice = input("What language?: ")
+    if choice == "1":
+        sentence_japanese()
+        choice == input("Would you like to translate another sentence? yes or no ")
+        if choice.lower == "no".lower:
+            choice = "end"
+            print("Quitting program...")
+    elif choice == "2":
+        sentence_french()
+        if choice.lower == "no".lower:
+            choice == "end"
+            print("Quitting program...")
+    elif choice == "3":
+        sentence_german()
+        if choice.lower == "no".lower:
+            choice == "end"
+            print("Quitting program...")
     else:
-        print("Quitting program...")
-        exit()
-
-main()
+        print("Please enter a number from 1-3 and try again.")
